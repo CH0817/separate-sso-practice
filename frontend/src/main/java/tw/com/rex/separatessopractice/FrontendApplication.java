d419926d871d543d392d48b57729abbc3e7f6a7a
RexYu <yu.chenhang@gmail.com>
2021-11-11 13:39:19 +0800

    init

---
+++ frontend/src/main/java/tw/com/rex/separatessopractice/FrontendApplication.java
@@ -0,0 +1,13 @@
+package tw.com.rex.separatessopractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendApplication.class,args);
+    }
+
+}
