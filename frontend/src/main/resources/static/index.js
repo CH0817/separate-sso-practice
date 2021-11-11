a61fdfed9f5a16875950656f34fa1a8d44c997e8
RexYu <yu.chenhang@gmail.com>
2021-11-11 16:14:45 +0800

    增加 spring cloud gateway

--- frontend/src/main/resources/static/index.js
+++ frontend/src/main/resources/static/index.js
@@ -5,19 +5,19 @@ $.ajaxSetup({
     error: function (jqXHR) {
         // 401 未認證，轉至 CAS，service 必須是後端 spring security 的 CAS login URL，renew 不太懂是什麼意思
         if (jqXHR.status === 401) {
-            window.location.replace(encodeURI('https://cas.example.org:8443/cas/login?service=http://localhost:8200/back/login/cas&renew=false'));
+            window.location.replace(encodeURI('https://cas.example.org:8443/cas/login?service=http://localhost:8300/back/login/cas&renew=false'));
         }
     }
 });
 
 function getUserDetails() {
     $.ajax({
-        url: 'http://localhost:8200/back/test/userDetails',
+        url: 'http://localhost:8300/back/test/userDetails',
         type: 'post',
         cache: false,
         success: function (data) {
             console.info(data);
-            $('#responseBody').text(data);
+            $('#responseBody').text('username: ' + data.username);
         }
     });
 }
---
+++ gateway/src/main/java/tw/com/rex/separatessopractice/GatewayApplication.java
@@ -0,0 +1,13 @@
+package tw.com.rex.separatessopractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
