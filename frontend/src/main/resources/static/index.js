b3553a8db162344c7581a2e1315a34e94800f6ad
Rex Yu <yu.chenhang@gmail.com>
2021-11-24 01:14:25 +0800

    重現 401 錯誤

--- frontend/src/main/resources/static/index.js
+++ frontend/src/main/resources/static/index.js
@@ -5,19 +5,31 @@ $.ajaxSetup({
     error: function (jqXHR) {
         // 401 未認證，轉至 CAS，service 必須是後端 spring security 的 CAS login URL，renew 不太懂是什麼意思
         if (jqXHR.status === 401) {
-            window.location.replace(encodeURI('https://cas.example.org:8443/cas/login?service=http://localhost:8300/back/login/cas&renew=false'));
+            window.location.replace(encodeURI('http://localhost:8080/cas/login?service=http://localhost:8200/back/login/cas'));
         }
     }
 });
 
-function getUserDetails() {
+function getApp1UserDetails() {
     $.ajax({
-        url: 'http://localhost:8300/back/test/userDetails',
+        url: 'http://localhost:8200/back/test/userDetails',
         type: 'post',
         cache: false,
         success: function (data) {
             console.info(data);
             $('#responseBody').text('username: ' + data.username);
+        }
+    });
+}
+
+function getApp2UserDetails() {
+    $.ajax({
+        url: 'http://localhost:8400/back/test/userDetails',
+        type: 'post',
+        cache: false,
+        success: function (data) {
+            console.info(data);
+            $('#responseBody2').text('username: ' + data.username);
         }
     });
 }
